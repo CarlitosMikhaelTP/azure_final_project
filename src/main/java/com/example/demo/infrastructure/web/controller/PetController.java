cadb105af5ce8d4a1c1d778ff082c84c263aaa57
CarlitosMikhaelTP <130238749+CarlitosMikhaelTP@users.noreply.github.com>
2024-12-10 02:20:49 -0500

    Agregando funcionalidad de mostrar fotos de las mascotas

--- src/main/java/com/example/demo/infrastructure/web/controller/PetController.java
+++ src/main/java/com/example/demo/infrastructure/web/controller/PetController.java
@@ -1,14 +1,25 @@
 package com.example.demo.infrastructure.web.controller;
 
+import com.example.demo.application.exceptions.WalkerExceptions.NotFound.WalkerNotFoundException;
 import com.example.demo.application.service.PetService.PetService;
+import com.example.demo.domain.entity.pet.Pet;
+import com.example.demo.domain.entity.walker.Walker;
+import com.example.demo.domain.repository.Pet.PetRepository;
 import com.example.demo.infrastructure.web.projection.classBased.PetDTO;
 import com.example.demo.infrastructure.web.projection.interfaceBased.PetProjection;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,6 +30,7 @@ public class PetController {
 
     // Inicializamos las clases para trabajar con este constructor con "final"
     private final PetService petService;
+    private final PetRepository petRepository;
 
     // Endpoint para registrar Pet
     @PostMapping("/register")
@@ -71,6 +83,33 @@ public class PetController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Error al actualizar la foto del mascota: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/foto/{id}")
+    public ResponseEntity<Resource> obtenerFotoPet(@PathVariable Integer id) {
+        Pet pet = petRepository.findById(id)
+                .orElseThrow(() -> new WalkerNotFoundException("Walker not found"));
+
+        String rutaFoto = pet.getFoto();
+
+        // Cargar el archivo de la ruta
+        Path filePath = Paths.get(rutaFoto);
+        Resource resource = null;
+
+        try {
+            resource = new UrlResource(filePath.toUri());
+            if (resource.exists() && resource.isReadable()) {
+                return ResponseEntity.ok()
+                        .contentType(MediaType.IMAGE_JPEG) // Cambiar el tipo MIME según el tipo de archivo
+                        .body(resource);
+            } else {
+                throw new FileNotFoundException("Was not possible find or read the pet's image");
+            }
+        } catch (MalformedURLException | FileNotFoundException e) {
+            // Manejo de excepciones si el archivo no se encuentra o no se puede leer
+            // Devolver un error o un recurso vacío en la respuesta
+            return ResponseEntity.notFound().build();
         }
     }
 }
